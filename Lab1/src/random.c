a1de1fb083258872a94399b9b0becfe975a2a3f0
Eric <eric@Laptop-liten.lol>
2021-09-08 15:52:30 +0200

    slab1 final
    new randomer random

--- Lab1/src/random.c
+++ Lab1/src/random.c
@@ -2,14 +2,16 @@
 
 int getRandomRange(int min, int max)
 {
-	int random = esp_random() % max;
+//	int random = esp_random() % max;
+//
+//	if(random > min)
+//	{
+//		return random;
+//	}
+//	else
+//	{
+//		return min;
+//	}
 
-	if(random > min)
-	{
-		return random;
-	}
-	else
-	{
-		return min;
-	}
+	return (esp_random() % (max- min) ) +min;
 }
